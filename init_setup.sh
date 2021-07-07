0cd2cbd32ce1b4008d7f76bc10eb2e7d98158294
Hector <66431870+Hector242@users.noreply.github.com>
2021-07-07 18:02:27 -0300

    Update init_setup.sh

--- init_setup.sh
+++ init_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir WorkDir
+mv Dockerfile WorkDir
+sudo mkdir /srv/gitlab
 export GITLAB=/srv/gitlab
-sudo docker build -t dockerfile_gitlab/gitlab01:latest .
-sudo docker run --name gitlab --hostname gitlab.test.com -p 443:443 -p 80:80 -d dockerfile_gitlab/gitlab01:latest
