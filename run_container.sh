c72f1ea6c41a6055d4ee84dc4f239a01433e2853
Hector <66431870+Hector242@users.noreply.github.com>
2021-07-07 18:24:34 -0300

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker build -t dockerfile_gitlab/gitlab01 WorkDir
+sudo docker build -t dockerfile_gitlab/gitlab01 ./WorkDir
 sudo docker run --name gitlab --hostname gitlab.test.com -p 443:443 -p 80:80 -d dockerfile_gitlab/gitlab01:latest
